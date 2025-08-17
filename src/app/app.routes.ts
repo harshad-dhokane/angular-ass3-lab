d5bd0abeea3ea790d6ee136894f1ee774e68b44e
harshad-dhokane <worrk.harsshad@gmail.com>
2025-08-17 23:20:34 +0530

    2nd Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { StudentsComponent } from './students/students';
+import {NotepadComponent} from './notepad/notepad'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'students', pathMatch: 'full' },
-  { path: 'students', component: StudentsComponent }
+  { path: 'students', component: StudentsComponent },
+  { path: 'notepad', component: NotepadComponent }
 ];
