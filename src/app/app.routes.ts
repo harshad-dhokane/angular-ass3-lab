5c675a7135541f16fc288884c88896bfe49a546d
harshad-dhokane <worrk.harsshad@gmail.com>
2025-08-17 23:13:59 +0530

    1st Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StudentsComponent } from './students/students';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'students', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent }
+];
